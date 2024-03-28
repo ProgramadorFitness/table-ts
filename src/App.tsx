ccb8515a0a00d00fa34967a2785c9c1c1bdcd513
ProgramadorFitness <139084638+ProgramadorFitness@users.noreply.github.com>
2024-03-28 00:43:29 -0500

    gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactPaginate from 'react-paginate';
+import data2 from './data.json'
 
 function App() {
+
+  const data = data2
+
+  const [paginaACtual, setCurrentPage] = useState(0);
+  const itemPorPagina: number = 10;
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <h1 className='title'>Table Dynamic</h1>
+      </div>
+
+      <div>
+        <table className='table'>
+          <thead className='thead'>
+            <tr>
+              <th>Nombre</th>
+              <th>Apellido</th>
+              <th>Ciudad</th>
+              <th>Telefono</th>
+              <th>Direccion</th>
+            </tr>
+          </thead>
+          <tbody className='tbody'>
+            {data.slice(paginaACtual * itemPorPagina, (paginaACtual + 1) * itemPorPagina).map((item) => (
+              <tr>
+                <td className='td'>{item.nombre}</td>
+                <td>{item.apellido}</td>
+                <td>{item.ciudad}</td>
+                <td>{item.telefono}</td>
+                <td>{item.direccion}</td>
+              </tr>
+            ))}
+          </tbody>
+          
+        </table>
+        <ReactPaginate
+        className='paginate'
+        previousLabel={'anterior'}
+        nextLabel={'siguiente'}
+        breakLabel={'...'}
+        pageCount={Math.ceil((data?.length || 0) / itemPorPagina)}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        onPageChange={({ selected }) => setCurrentPage(selected)}
+        containerClassName={''}
+        activeClassName={'paginateActive'}
+        pageClassName={'pageclass'}
+        previousClassName={'nextandprevius'}
+        nextClassName={'nextandprevius'} />
+      </div>
+      
     </div>
   );
 }
